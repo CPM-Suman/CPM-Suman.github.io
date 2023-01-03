03258631e168154afa2e0035346d2d936c6e0674
CPM-Suman <121448751+CPM-Suman@users.noreply.github.com>
2023-01-03 09:36:56 +0545

    Add files via upload

---
+++ 12-29 js/form.js
@@ -0,0 +1,25 @@
+let button = document.getElementById("sbt-btn");
+button.disabled = true;
+
+const email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const url_regex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+function validateForm() {
+  let name = document.getElementById("field1").value;
+  let email = document.getElementById("field2").value;
+  let number = document.getElementById("field3").value;
+  let website = document.getElementById("field4").value;
+
+  button.disabled = true;
+
+  if (name != "" && email != "" && number != "" && website != "") {
+    if (number.length === 10) {
+      if (email_regex.test(email)) {
+        if (url_regex.test(website)) {
+          button.disabled = false;
+        }
+      }
+    }
+  }
+}
